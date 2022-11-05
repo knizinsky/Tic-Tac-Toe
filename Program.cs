ef12336507611c2fa128e3d549bb9dde8c3d5cea
konrad.nizinski <konrad.nizinski@NY-18>
2022-11-05 12:48:40 +0100

    Dodaj pliki projektów.

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+namespace TicTacToe
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char[,] startBoard =
+            {
+                {'1','2','3'},
+                {'4','5','6'},
+                {'7','8','9'}
+            };
+            char[,] gameBoard = startBoard.Clone() as char[,];
+
+            bool player1Won = false;
+            bool player2Won = false;
+            bool nextIsPlayer1 = true;
+
+            //////////////////////////////////////////////
+
+            // Loop over rounds
+            for (int round = 0; round < gameBoard.Length; round++)
+            {
+                Console.Clear();
+                // TODO: draw the board
+                if (nextIsPlayer1)
+                {
+                    // TODO: player 1 move
+                    nextIsPlayer1 = false;
+                }
+                else
+                {
+                    // TODO: player 2 move
+                    nextIsPlayer1 = true;
+                }
+                if (player1Won || player2Won)
+                    break;
+            }
+
+           ///////////////////////////////////////////////
+
+            //End the game
+            Console.Clear();
+            // TODO: draw the board
+            Console.Write("Game ended! ");
+            // TODO: print who won
+
+        }
+    }
+}
